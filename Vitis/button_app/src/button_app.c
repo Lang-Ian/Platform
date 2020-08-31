4aab31d4909e36df030c7f2fe1968abef7dde3a2
Ian Lang <Ian.Lang@outlook.com>
2020-08-31 21:34:54 +0200

    Just made some cosmetic mods to familiarise myself which file is which!

--- Vitis/button_app/src/button_app.c
+++ Vitis/button_app/src/button_app.c
@@ -1,34 +1,10 @@
-/******************************************************************************
-*
-* Copyright (C) 2009 - 2014 Xilinx, Inc.  All rights reserved.
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* Use of the Software is limited solely to applications:
-* (a) running on a Xilinx device, or
-* (b) that interact with a Xilinx device through a bus or interconnect.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-* XILINX  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
-* OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-* SOFTWARE.
-*
-* Except as contained in this notice, the name of the Xilinx shall not be used
-* in advertising or otherwise to promote the sale, use or other dealings in
-* this Software without prior written authorization from Xilinx.
-*
-******************************************************************************/
+/*
+ * APP NUMBER 1
+ * What this app does:
+ * 1)  Flashes all four LEDs
+ * 3)  Samples all four buttons and prints out a message.
+ */
+
 
 #include <stdio.h>
 #include "platform.h"
@@ -41,7 +17,7 @@
 static XGpio   GPIOInstance_Ptr;
 static XGpioPs psGpioInstancePtr;
 
-static int InterruptFlag;
+//static int InterruptFlag;
 //const u32 iPinNumber        = 10;
 const u32 iPinNumberEMIO    = 54;    // https://forums.xilinx.com/t5/Embedded-Development-Tools/EMIO-PIN-numbers-ZYNQ-platform/td-p/273212
 const u32 iPinNumberEMIO1    = 55;
@@ -73,7 +49,7 @@ int main()  // UG585, General Purpose I/O
 {
     init_platform();
 
-    print( "Switch Polling Application\n\r" );
+    print( "APPLICATION NUMBER 1:  Switch Polling\n\r" );
 
 
     // AXI GPIO
