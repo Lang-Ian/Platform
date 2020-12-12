0045c30a0c7274480989f5bbf300efa7564ec1fe
Ian Lang <Ian.Lang@outlook.com>
2020-12-12 14:56:47 +0100

    The four LEDs are now controlled by the four push buttons.  It works well.  Except, I haven't yet figured out why it won't work when I invoke the .elf directly on the Ubuntu target; it only works through Vitis and the tcf server.

--- vitabox/gpio/src/gpio.c
+++ vitabox/gpio/src/gpio.c
@@ -6,7 +6,6 @@
 //                Nevertheless, it's good to know where the driver iformation is at last.
 // This is a user-space program.
 // To do:
-// open a different file pointer for the LEDs and for the buttons
 
 #include <stdio.h>
 #include <stdio.h>
@@ -23,9 +22,10 @@ int main()
     struct gpioline_info linfo;
     struct gpiohandle_request req2,  req3,  req4;
     struct gpiohandle_data    data2, data3, data4;
+
     int flash = 0;
 
-	printf( "Accessing LEDs\n");
+//	printf( "Accessing LEDs\n");
 
     int fd2 = open( "/dev/gpiochip2", 0 );
     (void) ioctl( fd2, GPIO_GET_CHIPINFO_IOCTL, &cinfo );
@@ -33,7 +33,7 @@ int main()
 
     linfo.line_offset = 1;
     (void) ioctl( fd2, GPIO_GET_LINEINFO_IOCTL, &linfo );
-    fprintf( stdout, "line %2d: %s\n", linfo.line_offset, linfo.name );
+//    fprintf( stdout, "line %2d: %s\n", linfo.line_offset, linfo.name );
 
     int fd3 = open( "/dev/gpiochip3", 0 );
     (void) ioctl( fd3, GPIO_GET_CHIPINFO_IOCTL, &cinfo );
@@ -41,7 +41,7 @@ int main()
 
     linfo.line_offset = 1;
     (void) ioctl( fd3, GPIO_GET_LINEINFO_IOCTL, &linfo );
-    fprintf( stdout, "line %2d: %s\n", linfo.line_offset, linfo.name );
+//    fprintf( stdout, "line %2d: %s\n", linfo.line_offset, linfo.name );
 
     int fd4 = open( "/dev/gpiochip4", 0 );
     (void) ioctl( fd4, GPIO_GET_CHIPINFO_IOCTL, &cinfo );
@@ -49,9 +49,7 @@ int main()
 
     linfo.line_offset = 1;
     (void) ioctl( fd4, GPIO_GET_LINEINFO_IOCTL, &linfo );
-    fprintf( stdout, "line %2d: %s\n", linfo.line_offset, linfo.name );
-
-//    return 1;
+//    fprintf( stdout, "line %2d: %s\n", linfo.line_offset, linfo.name );
 
     req2.lineoffsets[0] = 0;
     req2.lineoffsets[1] = 1;
@@ -77,14 +75,14 @@ int main()
     (void) ioctl( fd2, GPIO_GET_LINEHANDLE_IOCTL, &req2 );
     (void) ioctl( req2.fd, GPIOHANDLE_GET_LINE_VALUES_IOCTL, &data2 );
 
-    printf( "line 0  is %s ", data2.values[0]? "high" : "low " );
-    printf( "line 1  is %s ", data2.values[1]? "high" : "low " );
+//    printf( "line 0  is %s ", data2.values[0]? "high" : "low " );
+//    printf( "line 1  is %s ", data2.values[1]? "high" : "low " );
 
 
 
     // read PS switches
-    data4.values[0] = 1;  // 54 is this weird offset that applies to the PS side GPIOs.
-    data4.values[0] = 1;
+    data4.values[54] = 1;  // 54 is this weird offset that applies to the PS side GPIOs.
+    data4.values[55] = 1;
 
     req4.lines = 2;  // Both push buttons
     req4.flags = GPIOHANDLE_REQUEST_INPUT;
@@ -93,8 +91,8 @@ int main()
     (void) ioctl( fd4, GPIO_GET_LINEHANDLE_IOCTL, &req4 );
     (void) ioctl( req4.fd, GPIOHANDLE_GET_LINE_VALUES_IOCTL, &data4 );
 
-    printf( "line 54 is %s ",  data4.values[0]? "high" : "low " );
-    printf( "line 55 is %s\r", data4.values[1]? "high" : "low " );
+//    printf( "line 54 is %s ",  data4.values[0]? "high" : "low " );
+//    printf( "line 55 is %s\r", data4.values[1]? "high" : "low " );
 
 
 
@@ -104,14 +102,14 @@ int main()
     strcpy( req3.consumer_label, "PL blinker" );
 
     flash = ~flash;
-    data3.values[0] = flash;
-    data3.values[1] = ~flash;
-    data3.values[2] = flash;
-    data3.values[3] = ~flash;
+    data3.values[0] = data4.values[1];
+    data3.values[1] = data4.values[0];
+    data3.values[2] = data2.values[1];
+    data3.values[3] = data2.values[0];
 
     (void) ioctl( fd3, GPIO_GET_LINEHANDLE_IOCTL, &req3 );
     (void) ioctl( req3.fd, GPIOHANDLE_SET_LINE_VALUES_IOCTL, &data3 );
-   }
+    }
 
     return 0;
 }
